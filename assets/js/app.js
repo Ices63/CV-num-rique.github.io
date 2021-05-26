003131c2d4c141ce0cacb16a911c3eb8b84aec3b
Ices63 <77441096+Ices63@users.noreply.github.com>
2021-05-26 16:49:57 +0200

    NavBar + Background update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,27 @@
 let brice = document.getElementById('briceContainer');
 let langage = document.getElementById('langageContainer');
-let portfolio = document.getElementById('portfolioContainer');
+let skills = document.getElementById('skillsContainer');
 let contact = document.getElementById('contactContainer');
 
 langage.style.display = "none";
-portfolio.style.display = "none";
+skills.style.display = "none";
 
 document.getElementById('brice').addEventListener('click',function () {
     brice.style.display = "block";
     langage.style.display = "none";
-    portfolio.style.display = "none";
+    skills.style.display = "none";
 });
 
 document.getElementById('langage').addEventListener('click',function () {
     brice.style.display = "none";
     langage.style.display = "block";
-    portfolio.style.display = "none";
+    skills.style.display = "none";
 });
 
-document.getElementById('portfolio').addEventListener('click',function () {
+document.getElementById('skills').addEventListener('click',function () {
     brice.style.display = "none";
     langage.style.display = "none";
-    portfolio.style.display = "block";
+    skills.style.display = "block";
 });
 
 document.getElementById('contact').addEventListener('clic',function() {
